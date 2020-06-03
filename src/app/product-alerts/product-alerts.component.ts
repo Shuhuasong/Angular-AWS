ba12bc8598739aca209097931495bad170f308cc
Shuhuasong <shuhuasong888@gmail.com>
2020-06-02 20:22:29 -0400

    Store App

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -9,9 +9,10 @@ import { Output, EventEmitter } from '@angular/core';
 export class ProductAlertsComponent implements OnInit {
    @Input() product;
    @Output() notify = new EventEmitter();
-  constructor() { }
 
-  ngOnInit() {
+   constructor() { }
+
+   ngOnInit() {
   }
 
 }
